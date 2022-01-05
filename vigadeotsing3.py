bcf8f590477568826faa86fae9b9b077745765eb
IljaTrunov <93324206+IljaTrunov@users.noreply.github.com>
2022-01-05 12:02:09 +0200

    Update vigadeotsing3.py

--- vigadeotsing3.py
+++ vigadeotsing3.py
@@ -35,3 +35,52 @@ def vahetus(a:int,b:int):
     """
     abi=a
 
+def generaator(n:int,loend,a:int,b:int):
+    """ generiruet spisk
+    :param int n: koli4estvo tsifr
+    :param list loend: spisok v kotorom budut sgenerirovani tsifri
+    :param int a: minimalnoe zna4enie
+    :param int b: maksimalnoe zna4enie
+    """
+    for i in range (n):
+        loend.append(randint(a,b))
+    
+
+def jagamine(loend,p,n,nol):
+    """otdeljaet positivnie 4isla ot nigativnih i otdeljaet nuli v otdelnie spiski
+    :param list loend: spisk so vsemi zna4enijami
+    :param list p: spisk s positivom
+    :param list n: spisk s negativom
+    :param list nol: spisk s nuljami
+    """
+    for el in loend:
+        if el>0:
+            p.append(el)
+        elif el<0:
+            n.append(el)
+        else:
+            nol.append(el)
+
+def keskmine(loend):
+    """nahodit srednie zna4enie v spiske
+    vosvrashaet srednie zna4enie
+    :param list loend: spisok v kotorom ishut srednie 4islo
+    rtype: float
+    """
+    n=len(loend)
+    if n==0:
+        kesk=0
+    else:
+        sum=0
+        for i in loend:
+            sum+=i
+        kesk=round(sum/n,2)
+    return kesk
+
+def lisamine(loend,el:float):
+    """dobavljaet zadanoe zna4enie v zadanii spisk spisok
+    :param list loend: spisok v kotorii dobavitsja zna4enie
+    :param float el: element kotorii dobavitsja v spisk
+    """
+    loend.append(el)
+    loend.sort()
